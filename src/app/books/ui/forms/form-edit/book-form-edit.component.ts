4e5e8c3775778d99af36198b0a49ae6b801e0eda
Maciej Wilk <maciejw@apr.local>
2022-03-15 10:36:47 +0100

    Walidacja formularza edycji ksiazki

--- src/app/books/ui/forms/form-edit/book-form-edit.component.ts
+++ src/app/books/ui/forms/form-edit/book-form-edit.component.ts
@@ -3,6 +3,7 @@ import { Component, Inject, LOCALE_ID, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Book } from 'src/app/books/domain-model/book.model';
 import { BookService } from 'src/app/books/services/book.service';
+import { BookValidatorService } from 'src/app/books/services/validation/book-validator.service';
 
 @Component({
   selector: 'app-book-form-edit',
@@ -11,6 +12,7 @@ import { BookService } from 'src/app/books/services/book.service';
 export class BookFormEditComponent implements OnInit {
 
   constructor(readonly bookSrv: BookService,
+              readonly bookValidateSrv: BookValidatorService,
               private _route: ActivatedRoute,
               private _router: Router,
               @Inject(LOCALE_ID) private locale: string
@@ -19,6 +21,11 @@ export class BookFormEditComponent implements OnInit {
   id: string = '';
   date: string = '';
   book!: Book;
+  idError: boolean = true;
+  titleError: boolean = true;
+  authorError: boolean = true;
+  typeError: boolean = true;
+  dateError: boolean = true;
 
   ngOnInit(): void {
     this.id = this._route.snapshot.params['id'];
@@ -27,8 +34,16 @@ export class BookFormEditComponent implements OnInit {
   }
 
   onSubmit(data: Book): void {
-    this.bookSrv.updateBook(data.id,data);
-    this._router.navigate(['/books'])
+    //this.idError = this.bookValidateSrv.idValidation(data.id);
+    this.titleError = this.bookValidateSrv.dataValidation(data.title);
+    this.authorError = this.bookValidateSrv.dataValidation(data.author);
+    this.typeError = this.bookValidateSrv.dataValidation(data.type);
+    this.dateError = this.bookValidateSrv.dateValidation(data.releaseDate);
+    
+    if(this.idError && this.titleError && this.authorError && this.typeError && this.dateError) {
+      this.bookSrv.updateBook(data.id,data);
+      this._router.navigate(['/books'])
+    } 
   }
 
 }
