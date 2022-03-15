8de60cd95d2fe058b4b420fc310dd31ba9db8601
Maciej Wilk <maciejw@apr.local>
2022-03-15 10:31:32 +0100

    Walidacja formularza edycji ucznia

--- src/app/students/ui/forms/student-form-edit/student-form-edit.component.ts
+++ src/app/students/ui/forms/student-form-edit/student-form-edit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Student } from 'src/app/students/domain-models/student.model';
 import { StudentService } from 'src/app/students/services/students.service';
+import { StudentValidatorService } from 'src/app/students/services/validation/students-validator.service';
 
 @Component({
   selector: 'app-student-form-edit',
@@ -10,12 +11,18 @@ import { StudentService } from 'src/app/students/services/students.service';
 export class StudentFormEditComponent implements OnInit {
 
   constructor(readonly studentSrv: StudentService,
+              private studentValidateSrv: StudentValidatorService,
               private _route: ActivatedRoute,
               private _router: Router
   ) {}
 
   id: string = '';
   student!: Student;
+  idError: boolean = true;
+  nameError: boolean = true;
+  surnameError: boolean = true;
+  ageError: boolean = true;
+  classError: boolean = true;
 
   ngOnInit(): void {
     this.id = this._route.snapshot.params['id'];
@@ -23,8 +30,15 @@ export class StudentFormEditComponent implements OnInit {
   }
 
   onSubmit(data: Student): void {
-    this.studentSrv.updateStudent(data.id,data);
-    this._router.navigate(['/students'])
-  }
+    //this.idError = this.studentValidateSrv.idValidation(data.id);
+    this.nameError = this.studentValidateSrv.nameValidation(data.name);
+    this.surnameError = this.studentValidateSrv.surnameValidation(data.surname);
+    this.ageError = this.studentValidateSrv.ageValidation(data.age);
+    this.classError = this.studentValidateSrv.classValidation(data.class);
 
+    if(this.idError && this.nameError && this.surnameError && this.ageError && this.classError) {
+      this.studentSrv.updateStudent(data.id,data);
+      this._router.navigate(['/students'])
+    }    
+  }
 }
